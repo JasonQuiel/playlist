975bca4fdf9d9ccc2e5188da330f04e3bbb17d96
Jason <Jason-Quiel@live.nmit.ac.nz>
2021-09-21 13:19:15 +1200

    ali player

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const musicContainer = document.getElementById('music-container')
+
+const playBtn = document.getElementById('play')
+const preBtn = document.getElementById('prev')
+const nextBtn = document.getElementById('next')
+
+const audio = document.getElementById('audio')
+const progress = document.getElementById('progress')
+const progress = document.getElementById('progress-container')
+
+const title = document.getElementById('title')
+const cover = document.getElementById('cover')
+
+const songs = []
+
+let songIndex = 2;
+
+loadSong
+
+
+
+function updateProgress() {
+    const duration, currentDuration = e.scrElement
+}
